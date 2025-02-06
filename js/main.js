f9183b1e6b0848ebb5e49ad319cb877818140c05
Qwayb <girdden@gmail.com>
2025-02-06 19:13:03 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+new Vue({
+    el: "#app",
+    data: {
+        columns: [
+            { id: 1, title: "Столбец 1", cards: [] },
+            { id: 2, title: "Столбец 2", cards: [] },
+            { id: 3, title: "Столбец 3", cards: [] }
+        ]
+    }
+});
