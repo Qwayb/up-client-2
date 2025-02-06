f36cee895ab714f16d1d84de330609b0085e5285
Qwayb <girdden@gmail.com>
2025-02-06 19:50:57 +0700

    you can adding notes to the 1st column

--- js/main.js
+++ js/main.js
@@ -2,9 +2,47 @@ new Vue({
     el: "#app",
     data: {
         columns: [
-            { id: 1, title: "Столбец 1", cards: [] },
+            {
+                id: 1,
+                title: "Столбец 1",
+                cards: [
+                    {
+                        id: "phantom",
+                        title: "",
+                        items: [],
+                        completedAt: null,
+                        isPhantom: true,
+                    },
+                ],
+            },
             { id: 2, title: "Столбец 2", cards: [] },
-            { id: 3, title: "Столбец 3", cards: [] }
-        ]
-    }
+            { id: 3, title: "Столбец 3", cards: [] },
+        ],
+    },
+    methods: {
+        checkPhantomCard() {
+            const firstColumn = this.columns[0];
+            const phantomCard = firstColumn.cards.find((c) => c.isPhantom);
+
+            if (phantomCard && phantomCard.title.trim() && phantomCard.items.length >= 3) {
+                phantomCard.id = Date.now();
+                phantomCard.isPhantom = false;
+
+                firstColumn.cards.push({
+                    id: "phantom",
+                    title: "",
+                    items: [],
+                    completedAt: null,
+                    isPhantom: true,
+                });
+            }
+        },
+        addItemToPhantom() {
+            const phantomCard = this.columns[0].cards.find((c) => c.isPhantom);
+            if (phantomCard) {
+                phantomCard.items.push({ text: "", done: false });
+                this.checkPhantomCard();
+            }
+        },
+    },
 });
